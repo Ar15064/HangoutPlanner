a8bc02d63a7b6c0ea06e76401c38bbee2aeb8426
zeshan0603 <113106007+zeshan0603@users.noreply.github.com>
2023-09-08 09:37:58 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("testing again");
         System.out.println("testing agian");
 
-        System.out.println("I forked it ");
+        System.out.println("I forked it   ");
     }
 }
